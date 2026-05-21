e5aa05441b75b0aaf925e31e61349e3a0e554565
gabrielgallan <gabriel31.gal@gmail.com>
2026-05-21 00:57:47 -0300

    add dashboard cards

--- src/pages/customer/overview/index.tsx
+++ src/pages/customer/overview/index.tsx
@@ -1,5 +1,4 @@
 import { PageTitle } from '@/components/page-title'
-import { CardTest } from './components/card'
 
 export function Overview() {
 	return (
@@ -7,15 +6,6 @@ export function Overview() {
 			<PageTitle title="Overview" />
 			<div className="max-w-300 mx-auto space-y-4">
 				<h1 className="text-3xl font-bold tracking-tight">Overview</h1>
-
-				<div className="grid grid-cols-4 gap-4">
-					<CardTest />
-					<CardTest />
-					<CardTest />
-					<CardTest />
-				</div>
-
-				<div className="grid grid-cols-9 gap-4"></div>
 			</div>
 		</>
 	)
--- src/pages/org/dashboard/index.tsx
+++ src/pages/org/dashboard/index.tsx
@@ -1,11 +1,21 @@
 import { PageTitle } from '@/components/page-title'
+import { CardTest } from './components/card'
 
 export function Dashboard() {
 	return (
 		<>
 			<PageTitle title="Dashboard" />
-			<div className="max-w-300 mx-auto">
-				<h1 className="text-3xl font-bold tracking-tight">Overview</h1>
+			<div className="max-w-300 mx-auto space-y-8 mt-8">
+				<h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+				<div className="grid gap-4 grid-cols-4">
+					<CardTest />
+					<CardTest />
+					<CardTest />
+					<CardTest />
+				</div>
+
+				<div className="grid grid-cols-9 gap-4"></div>
 			</div>
 		</>
 	)
